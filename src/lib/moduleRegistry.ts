638dbe72b4c070c3948d43a58af842b8bd96303d
Allwin S Antony <santonyallwin@gmail.com>
2026-09-19 10:53:29 +0530

    refactor: remove components and maps modules along with loaders and datasets

--- src/lib/moduleRegistry.ts
+++ src/lib/moduleRegistry.ts
@@ -33,14 +33,10 @@ export interface ModuleManifest {
 import { fontsManifest } from '../modules/fonts/manifest';
 import { iconsManifest } from '../modules/icons/manifest';
 
-import componentsManifest from '@/modules/components/manifest';
-import mapsManifest from '@/modules/maps/manifest';
 import toolsManifest from '@/modules/tools/manifest';
 
 export const moduleRegistry: ModuleManifest[] = [
   fontsManifest,
   iconsManifest,
-  componentsManifest,
-  mapsManifest,
   toolsManifest
 ];
